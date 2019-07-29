b40cfc796e2b5083151ead4327c0004fdf1765a6
Hiroki.T <hirodora@me.com>
2019-07-29 13:30:16 +0900

    change: 2d -> 3d

--- face-group.py
+++ face-group.py
@@ -9,6 +9,7 @@ from sklearn.decomposition import PCA
 from sklearn.cluster import DBSCAN
 import glob
 from matplotlib import pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 
 filelist = glob.glob('./faces/*[jpg|JPG|png]')
@@ -16,29 +17,37 @@ images = [face_recognition.load_image_file(i) for  i in filelist]
 
 faces = []
 for i in images:
-    locations = face_recognition.face_locations(i, model='cnn')  # using cnn
-    faces += face_recognition.face_encodings(i, known_face_locations=locations)
+#     locations = face_recognition.face_locations(i, model='cnn')  # using cnn
+    faces += face_recognition.face_encodings(i)
 
 faces = np.array(faces)
-compressed = PCA(n_components=2).fit_transform(faces)
+compressed = PCA(n_components=3).fit_transform(faces)
 # compressed = TSNE(n_components=2, random_state=0, method='exact').fit_transform(n_10)
 
-db = DBSCAN().fit(faces)
+db = DBSCAN(eps=0.45, min_samples=1).fit(faces)
 labels = db.labels_
 cluster_max = np.max(labels)
+
+fig = plt.figure()
+ax = Axes3D(fig)
 
 #  グループごと点をプロット & print
 for i in range(0, cluster_max+1):
     points = compressed[labels == i]
-    plt.scatter(points[:, 0], points[:, 1])
+    # plt.scatter(points[:, 0], points[:, 1, points[:, 2]])
+    ax.scatter3D(points[:, 0], points[:, 1], points[:, 2])
+    print('\nGroup', i)
+    for i, v in enumerate(labels == i):
+        if v == True:
+            print(filelist[i])
 
 
 # ファイル名をプロット
-for i, xy in enumerate(compressed):
-    plt.annotate(filelist[i], xy)
+for i, (x, y, z) in enumerate(compressed):
+    ax.text(x, y, z, filelist[i].split('/')[-1])
 
 points = compressed[labels == -1]
-plt.scatter(points[:, 0], points[:, 1], color="gray")
+ax.scatter3D(points[:, 0], points[:, 1], points[:, 2], color="gray")
 print('\nNoise')
 for i, v in enumerate(labels == -1):
     if v == True:
